75b42dedfbb9d6d207444a208dfefeceb1db8be7
adux <adriangarate@protonmail.com>
2018-09-05 12:21:45 +0200

    JS for accounting

--- home/filters.py
+++ home/filters.py
@@ -1,17 +1,19 @@
 from .models import Accounting, Booking
+from django import forms
+from django.forms.widgets import SplitDateTimeWidget
 import django_filters
 
 
 class AccountingFilter(django_filters.FilterSet):
-    pay_date = django_filters.DateFromToRangeFilter()
-    pay_till = django_filters.DateFromToRangeFilter()
+    pay_date = django_filters.DateFromToRangeFilter(widget=SplitDateTimeWidget(attrs={'placeholder': 'YYYY-MM-DD'}))
+    pay_till = django_filters.DateFromToRangeFilter(widget=SplitDateTimeWidget(attrs={'placeholder': 'YYYY-MM-DD'}))
     class Meta:
         model = Accounting
         fields = ['category','event','methode','status']
 
 class BookingFilter(django_filters.FilterSet):
-    pay_date = django_filters.DateFromToRangeFilter()
-    pay_till = django_filters.DateFromToRangeFilter()
+    pay_date = django_filters.DateFromToRangeFilter(widget=SplitDateTimeWidget(attrs={'placeholder': 'YYYY-MM-DD'}))
+    pay_till = django_filters.DateFromToRangeFilter(widget=SplitDateTimeWidget(attrs={'placeholder': 'YYYY-MM-DD'}))
     class Meta:
         model = Booking
         fields = ['category','event','methode', 'status','abo','reduction']
