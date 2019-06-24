b13104bd0bc665f497bd778e0894be3cd1406a50
adux <adriangarate@protonmail.com>
2019-06-24 14:50:58 +0200

    Account Partner id modifi

--- apps/accounting/migrations/0002_auto_20190430_0821.py
+++ apps/accounting/migrations/0002_auto_20190430_0821.py
@@ -19,9 +19,9 @@ class Migration(migrations.Migration):
         #     name='booking',
         #     field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='booking.Booking'),
         # ),
-        migrations.AddField(
-            model_name='invoice',
-            name='partner',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='accounting.Partner'),
-        ),
+        # migrations.AddField(
+        #     model_name='invoice',
+        #     name='partner',
+        #     field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='accounting.Partner'),
+        # ),
     ]
