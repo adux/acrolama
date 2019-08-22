057b2c8ea8d1b9e0f76578cfae9b77dbd9c26ad0
adux <adriangarate@protonmail.com>
2019-08-22 11:38:20 +0200

    Config Settings for Allauth

--- acrolama/settings/production.py
+++ acrolama/settings/production.py
@@ -1,8 +1,10 @@
 from .base import *
-#Media and Static Files for production
+# Media and Static Files for production
 from acrolama.aws.conf import *
+# For Heroku
+import dj_database_url
 
-#Sentry SDK
+# Sentry SDK
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
@@ -22,7 +24,19 @@ ANYMAIL = {
     "MAILGUN_API_URL": os.environ.get('MAILGUN_API_URL', default='https://api.mailgun.net/v3'),
 }
 
-DEFAULT_FROM_EMAIL = 'noreply@acrolama.com'
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+
+# Config https://django-allauth.readthedocs.io/en/latest/configuration.html
+ACCOUNT_AUTHENTICATION_METHOD = os.environ.get('ACCOUNT_AUTHENTICATION_METHOD')
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = os.environ.get('ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS')
+ACCOUNT_EMAIL_REQUIRED = os.environ.get('ACCOUNT_EMAIL_REQUIRED')
+ACCOUNT_EMAIL_VERIFICATION = os.environ.get('ACCOUNT_EMAIL_VERIFICATION')
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = os.environ.get('ACCOUNT_LOGIN_ATTEMPTS_LIMIT')
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = os.environ.get('ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION')
+ACCOUNT_SESSION_REMEMBER = os.environ.get('ACCOUNT_SESSION_REMEMBER')
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = os.environ.get('ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE')
+ACCOUNT_SIGNUP_FORM_CLASS = os.environ.get('ACCOUNT_SIGNUP_FORM_CLASS')
+ACCOUNT_USERNAME_REQUIRED = os.environ.get('ACCOUNT_USERNAME_REQUIRED')
 
 ADMINS = (
     ('Adrian Garate', 'adrian@acrolama.com'),
@@ -37,10 +51,10 @@ DEBUG = False
 ALLOWED_HOSTS = ['stageacrolama.herokuapp.com', 'acrolama.herokuapp.com', 'www.acrolama.com', 'acrolama.com', '.acrolama.com']
 
 # Database for heroku
-import dj_database_url
+# import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-#DATABASES['default']['CONN_MAX_AGE'] = 500
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Static files (CSS, JavaScript, Images)
 STATICFILES_DIRS = [
@@ -48,14 +62,14 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
-#Admin URL
+# Admin URL
 ADMIN_URL = os.environ.get('DJANGO_ADMIN_URL')
 
-#### Secure
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+# Secure
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
