4c066fdffedc111b90de8edd322958deb733cd6c
adux <adriangarate@protonmail.com>
2019-08-28 08:11:55 +0200

    Solving settings problem

--- acrolama/settings/__init__.py
+++ acrolama/settings/__init__.py
@@ -1,7 +1,8 @@
 from .base import *
-from .production import *
-
 try:
     from .local import *
-except:
-    pass
+    live = False
+except ImportError:
+    live = True
+    if live:
+        from .production import *
