5175ea110b2b41ddf0eae49853d0b9d21c1d33b0
adux <adriangarate@protonmail.com>
2019-09-27 20:30:48 +0200

    Config TODO

--- acrolama/settings/base.py
+++ acrolama/settings/base.py
@@ -142,14 +142,14 @@ USE_TZ = True
 
 # Todo-specific settings
 INSTALLED_APPS += ["todo"]
-TODO_DEFAULT_LIST_ID = None
-# Restrict access to todo lists/views to `is_staff()` users.
-# False here falls back to `is_authenticated()` users.
+# Restrict access to ALL todo lists/views to `is_staff` users.
+# If False or unset, all users can see all views (but more granular permissions are still enforced
+# within views, such as requiring staff for adding and deleting lists).
 TODO_STAFF_ONLY = True
 
 # If you use the "public" ticket filing option, to whom should these tickets be assigned?
 # Must be a valid username in your system. If unset, unassigned tickets go to "Anyone."
-TODO_DEFAULT_ASSIGNEE = None
+TODO_DEFAULT_ASSIGNEE = 'adrian@acrolama.com'
 
 # If you use the "public" ticket filing option, to which list should these tickets be saved?
 # Defaults to first list found, which is probably not what you want!
@@ -158,4 +158,19 @@ TODO_DEFAULT_LIST_SLUG = 'tickets'
 # If you use the "public" ticket filing option, to which *named URL* should the user be
 # redirected after submitting? (since they can't see the rest of the ticket system).
 # Defaults to "/"
-TODO_PUBLIC_SUBMIT_REDIRECT = '/'
+TODO_PUBLIC_SUBMIT_REDIRECT = 'home'
+
+# Enable or disable file attachments on Tasks
+# Optionally limit list of allowed filetypes
+TODO_ALLOW_FILE_ATTACHMENTS = True
+TODO_ALLOWED_FILE_ATTACHMENTS = [".jpg", ".gif", ".csv", ".pdf", ".zip"]
+TODO_MAXIMUM_ATTACHMENT_SIZE = 5000000  # In bytes
+
+# additionnal classes the comment body should hold
+# adding "text-monospace" makes comment monospace
+TODO_COMMENT_CLASSES = []
+
+# The following two settings are relevant only if you want todo to track a support mailbox -
+# see Mail Tracking below.
+# TODO_MAIL_BACKENDS
+# TODO_MAIL_TRACKERS
