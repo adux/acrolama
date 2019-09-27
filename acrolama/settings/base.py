390fb651c0d38514ec9e6de2af219ad0a588c353
adux <adriangarate@protonmail.com>
2019-09-27 12:35:42 +0200

    Todo as a B repo, test

--- acrolama/settings/base.py
+++ acrolama/settings/base.py
@@ -6,6 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # Change of root of apps
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps/todo'))
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
