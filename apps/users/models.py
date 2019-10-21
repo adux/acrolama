c2947710f95c1427e9675b8a7b42cddac1012f91
adux <adriangarate@protonmail.com>
2019-10-21 15:00:22 +0200

    Email of user displyed

--- apps/users/models.py
+++ apps/users/models.py
@@ -82,9 +82,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         '''
         Returns the first_name plus the last_name, with a space in between.
         '''
-        full_name = '%s %s <%s>' % (
-            self.first_name, self.last_name, self.email
-        )
+        full_name = '%s %s ' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
@@ -100,4 +98,4 @@ class User(AbstractBaseUser, PermissionsMixin):
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
     def __str__(self):
-        return '%s %s' % (self.first_name, self.last_name)
+        return '%s %s <%s>' % (self.first_name, self.last_name, self.email)
