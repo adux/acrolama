ce8efbd29e2ca8f65b89ffc6a7b0b847803d0e41
adux <adriangarate@protonmail.com>
2020-01-23 20:52:46 +0100

    Changed description system

--- acrolama/settings/__init__.py
+++ acrolama/settings/__init__.py
@@ -1,8 +1,8 @@
 from .base import *
-# try:
-#     from .local import *
-#     live = False
-# except ImportError:
-#     live = True
-#     if live:
-from .production import *
+try:
+    from .local import *
+    live = False
+except ImportError:
+    live = True
+    if live:
+        from .production import *
