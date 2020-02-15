219fe6cc2e59375fea62d964207f63797b2a1ab2
adux <adriangarate@protonmail.com>
2020-02-15 14:52:32 +0100

    gzip

--- acrolama/settings/base.py
+++ acrolama/settings/base.py
@@ -16,6 +16,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps/todo'))
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
