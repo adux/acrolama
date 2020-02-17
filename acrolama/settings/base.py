3ecdc09ccb89ba831941006b7ad70b1ff0858164
adux <adriangarate@protonmail.com>
2020-02-17 11:42:57 +0100

    Django redis testing

--- acrolama/settings/base.py
+++ acrolama/settings/base.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps/todo'))
 
 MIDDLEWARE = [
-    'django.middleware.gzip.GZipMiddleware',
+    # 'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
