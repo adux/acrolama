db8876af5074273ee5f4256500b57a5c78d4e139
adux <adriangarate@protonmail.com>
2020-02-18 12:15:18 +0100

    :D

--- acrolama/settings/production.py
+++ acrolama/settings/production.py
@@ -66,7 +66,7 @@ ANYMAIL = {
 # Database for heroku
 # ------------------------------------------------------------------------------
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # HTML MINIFY
