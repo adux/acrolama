6ce033c069388b6d29b22a5d28010df6389ef010
adux <adriangarate@protonmail.com>
2020-09-24 11:45:43 +0200

    bla

--- home/models.py
+++ home/models.py
@@ -1,8 +1,14 @@
+import sys
+import os.path
+import PIL.Image
+
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.files.base import ContentFile
+
 from django.db import models
 from django.db.models.signals import pre_save
 from django.urls import reverse
-from django.utils.translation import ugettext as _
-
 
 from home.utils import unique_slug_generator
 
@@ -90,10 +96,64 @@ class NewsList(models.Model):
 
     event = models.ForeignKey("project.Event", on_delete=models.CASCADE)
     image = models.ImageField(upload_to="news/")
+    thumbnail = models.ImageField(upload_to="news/thumbs/", editable=False)
     added_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.event.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.image = self.compressImage(self.image)
+        if not self.image.closed:
+            if not self.make_thumbnail():
+                raise Exception("Could not create thumbnail - is the file type valid?")
+        super(NewsList, self).save(*args, **kwargs)
+
+    def compressImage(self, image):
+        imageTemproary = PIL.Image.open(image)
+        outputIoStream = BytesIO()
+        imageTemproary.thumbnail((900, 300), PIL.Image.ANTIALIAS)
+        imageTemproary.save(
+            outputIoStream, format="JPEG", quality=75, subsampling=0, optimize=True,
+        )
+        outputIoStream.seek(0)
+        image = InMemoryUploadedFile(
+            outputIoStream,
+            "ImageField",
+            "%s.jpg" % image.name.split(".")[0],
+            "image/jpeg",
+            sys.getsizeof(outputIoStream),
+            None,
+        )
+        return image
+
+    def make_thumbnail(self):
+        image = PIL.Image.open(self.image)
+        image.thumbnail((600, 200), PIL.Image.ANTIALIAS)
+        thumb_name, thumb_extension = os.path.splitext(self.image.name)
+        thumb_extension = thumb_extension.lower()
+        thumb_filename = thumb_name + "_thumb" + thumb_extension
+        if thumb_extension in [".jpg", ".jpeg"]:
+            FTYPE = "JPEG"
+        elif thumb_extension == ".gif":
+            FTYPE = "GIF"
+        elif thumb_extension == ".png":
+            FTYPE = "PNG"
+        else:
+            return False  # Unrecognized file type
+
+        # Save thumbnail to in-memory file as StringIO
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+
+        # set save=False, otherwise it will run in an infinite loop
+        self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+
+        temp_thumb.close()
+
+        return True
 
 
 class Portfolio(models.Model):
