97d8c06fbe7c7d789b219778ce7a760062ab5831
adux <adriangarate@protonmail.com>
2020-10-21 11:40:00 +0200

    Methode

--- apps/accounting/filters.py
+++ apps/accounting/filters.py
@@ -14,17 +14,14 @@ class AccountFilter(django_filters.FilterSet):
         queryset=Event.objects.all(), widget=autocomplete.ModelSelect2(url="event-autocomplete")
     )
     pay_till = django_filters.DateFilter(field_name="pay_till", method="filter_by_date_smaller_than", label="Pay till")
-
     balance = django_filters.ChoiceFilter(field_name="balance", choices=BALANCE, label="Account")
-
     start_pay_date = django_filters.DateFilter(field_name="pay_date", lookup_expr="gt", label="Start Pay date",)
-
     end_pay_date = django_filters.DateFilter(field_name="pay_date", lookup_expr="lt", label="End Pay date",)
     range_pay_date = django_filters.DateRangeFilter(field_name="pay_date", label="Range Pay date")
 
     class Meta:
         model = Invoice
-        fields = {"id", "status"}
+        fields = {"id", "status", "methode"}
 
     def filter_by_date_smaller_than(self, queryset, name, value):
         return queryset.filter(Q(pay_till__lte=value))
