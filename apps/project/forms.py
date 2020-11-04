5bd8375c8660ee3d62daf2cadd21cde4f4b29bb7
adux <adriangarate@protonmail.com>
2020-11-04 12:50:38 +0100

    Cache TEST

--- apps/project/forms.py
+++ apps/project/forms.py
@@ -4,7 +4,18 @@ from django import forms
 from django.core.cache import cache
 
 from users.models import User
-from project.models import Event, TimeLocation, PriceOption
+from project.models import (
+    Project,
+    Discipline,
+    Irregularity,
+    Event,
+    TimeLocation,
+    PriceOption,
+    Policy,
+)
+
+from audiovisual.models import Image, Video
+
 
 # Widgets
 from booking.widgets import BootstrapedSelect2Multiple
@@ -32,11 +43,33 @@ class EventUpdateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(EventUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['project'].queryset = cache.get_or_set(
+            'cache_project', Project.objects.all(), 120
+        )
         self.fields['time_locations'].queryset = cache.get_or_set(
             'cache_time_locations', TimeLocation.objects.all(), 120
         )
+        self.fields['irregularities'].queryset = cache.get_or_set(
+            'cache_irregularities', Irregularity.objects.all(), 120
+        )
         self.fields['price_options'].queryset = cache.get_or_set(
             'cache_price_options', PriceOption.objects.all(), 120
+        )
+        self.fields['policy'].queryset = cache.get_or_set(
+            'cache_policy', Policy.objects.all(), 120
+        )
+        self.fields['discipline'].queryset = cache.get_or_set(
+            'cache_discipline', Discipline.objects.all(), 120
+        )
+        self.fields['teachers'].queryset = cache.get_or_set(
+            'cache_user', User.objects.all(), 120
+        )
+        self.fields['team'].queryset = cache.get('cache_user')
+        self.fields['images'].queryset = cache.get_or_set(
+            'cache_images', Image.objects.all(), 120
+        )
+        self.fields['Video'].queryset = cache.get_or_set(
+            'cache_videos', Video.objects.all(), 120
         )
 
 
