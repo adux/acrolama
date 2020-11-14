ec9acdc0cb5b753aad3db41b72335724e189fb27
adux <adriangarate@protonmail.com>
2020-11-14 21:19:21 +0100

    Sentry update2

--- apps/project/forms.py
+++ apps/project/forms.py
@@ -84,8 +84,8 @@ class EventUpdateForm(forms.Form):
                 # If there is a cached version filter in python and dont hit db
                 if cached_query is not None:
                     selected_obj = [obj for obj in cached_query if obj.id in data[m2m_field.attname]]
-                    logging.info(data[m2m_field.attname])
-                    logging.info(cached_query)
+                    logging.error(data[m2m_field.attname])
+                    logging.error(cached_query)
                     logging.error(selected_obj)
                 else:
                     selected_obj = [
